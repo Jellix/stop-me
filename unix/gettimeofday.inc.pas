6e81816b964989e3c8cfe8748909022c8a31a45e
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2007-07-05 08:03:49 +0000

    * Fixed copyright declaration.  This  convinced  me  — after all —  that   merging-by-copy does NOT work. ;)

--- unix/gettimeofday.inc.pas
+++ unix/gettimeofday.inc.pas
@@ -1,6 +1,37 @@
 {/= GetTimeOfDay syscall =============================================\}
 {                                                                      }
-{ Copyright (C) 2007 by MDC Max Daetwyler AG, Bleienbach, Switzerland  }
+{ Provides the syscall to gettimeofday() either directly or via libc.  }
+{ Part of the Stop-Me (STOchastic Performance MEasurement) project.    }
+{                                                                      }
+{ Copyright (C) 2007 by Johnny L. Fencey                               }
+{                                                                      }
+{ This program is free software; you can redistribute it and/or modify }
+{ it under the terms of the GNU General Public License as published by }
+{ the  Free  Software  Foundation; either version 2 of the License, or }
+{ (at your option) any later version.                                  }
+{                                                                      }
+{ This  program is distributed in the hope that it will be useful, but }
+{ WITHOUT  ANY  WARRANTY;   without   even  the  implied  warranty  of }
+{ MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.   See the }
+{ GNU General Public License for more details.                         }
+{                                                                      }
+{ You  should  have  received a copy of the GNU General Public License }
+{ along with this program; if not, write to the                        }
+{ Free Software Foundation, Inc.                                       }
+{ 51 Franklin St, Fifth Floor                                          }
+{ Boston, MA  02110-1301                                               }
+{ USA                                                                  }
+{                                                                      }
+{ The original author can be reached via                               }
+{                                                                      }
+{ e-mail    : c.sucks@jlfencey.com                                     }
+{                                                                      }
+{ snail-mail: Mr. Vinzent Hoefler                                      }
+{             Drangsalengaessli 5                                      }
+{             CH-3360 Herzogenbuchsee                                  }
+{             Switzerland, Europe, Earth, Solar System, Milky Way      }
+{                                                                      }
+{ Special proprietary licenses may be available.                       }
 {                                                                      }
 {\====================================================================/}
 // Notes: Syscall (non-LibC version): {$INLINE} must be enabled.
