82a05fffdca80eb53247cd4fe4e755650a1585c2
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2007-07-09 10:40:35 +0000

    + Added "title" to be included in generated HTML documentation. - Removed absolute path to pasdoc from Win32 version of script.

--- gen_doc.bat
+++ gen_doc.bat
@@ -1,1 +1,1 @@
-c:\tools\pasdoc.exe -E html_doc -S documented_units --marker=-- --use-tipue-search --visible-members=published,public,protected,private -DPAS_DOC
+pasdoc.exe -E html_doc -S documented_units --title="stop-me Documentation" --marker=-- --use-tipue-search --visible-members=published,public,protected,private -DPAS_DOC
--- gen_doc.sh
+++ gen_doc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pasdoc -E html_doc -S documented_units --marker=-- --use-tipue-search --visible-members=published,public,protected,private -DPAS_DOC
+pasdoc -E html_doc -S documented_units --title="stop-me Documentation" --marker=-- --use-tipue-search --visible-members=published,public,protected,private -DPAS_DOC
