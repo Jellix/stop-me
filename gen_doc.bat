1b464f2a9d80f5ed35ec2d9da511c5a747a13a81
Vinzent "Jellix" Saranen <ada.rocks@jlfencey.com>
2009-04-20 10:41:29 +0000

    * Incorporated generation of code documentation into   Makefile.fpc. + Added graphviz-classes/dependencies to documentation.

--- gen_doc.bat
+++
@@ -1,1 +0,0 @@
-pasdoc.exe -E html_doc -S documented_units --title="stop-me Documentation" --marker=-- --use-tipue-search --visible-members=published,public,protected,private -DPAS_DOC
--- gen_doc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-pasdoc -E html_doc -S documented_units --title="stop-me Documentation" --marker=-- --use-tipue-search --visible-members=published,public,protected,private -DPAS_DOC
